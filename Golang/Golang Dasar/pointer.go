462a03e550cd1d7b27269b67136ad0fa3026dbbb
bayunashr <nashr.bayu77@gmail.com>
2022-08-15 18:27:47 +0700

    pointer function

--- Golang/Golang Dasar/pointer.go
+++ Golang/Golang Dasar/pointer.go
@@ -6,6 +6,10 @@ type Address struct {
 	City, Province, Country string
 }
 
+func change(x *Address) {
+	x.City = "Ekek"
+}
+
 func main() {
 
 	// address1 := Address{"Sidoarjo", "Jawa Timur", "Indonesia"}
@@ -15,14 +19,18 @@ func main() {
 	var address2 *Address = &address1
 	var address3 *Address = &Address{"Bali", "Jawa Barat", "Indonesia"}
 	var address4 *Address = new(Address)
+	var address5 Address = Address{"Batu", "Jawa Timur", "Indonesia"}
 
 	address2.Country = "Balkan"
 
 	*address2 = Address{"Surabaya", "Jawa Timur", "Indonesia"}
 
+	change(&address5)
+
 	fmt.Println(address1)
 	fmt.Println(address2)
 	fmt.Println(address3)
 	fmt.Println(address4)
+	fmt.Println(address5)
 
 }
